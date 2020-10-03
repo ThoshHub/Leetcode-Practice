4a00d26119b4848ca0011c5063ef011b6ed7be32
ThoshHub <thosh@DESKTOP-Q6PIMO2.localdomain>
2020-10-03 18:37:04 -0400

    changed naming scheme

---
+++ merge_two_sorted_lists.java
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/merge-two-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ 
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        
+    }
+}
