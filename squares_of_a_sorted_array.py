fa05d9b3d9e170f8f9545933ed143ce06cfffc0e
ThoshHub <thosh@DESKTOP-Q6PIMO2.localdomain>
2020-10-03 19:11:32 -0400

    added brute force method for Squares of Sorted Array

--- squares_of_a_sorted_array.py
+++ squares_of_a_sorted_array.py
@@ -1,1 +1,11 @@
-print("hello world")
+# https://leetcode.com/problems/squares-of-a-sorted-array/
+
+def sortedSquares(A):
+	B = []
+	for i in A:
+		B.append(i ** 2)
+	B.sort()
+	return B
+
+A = [-4,-1,0,3,10]
+sortedSquares(A)
