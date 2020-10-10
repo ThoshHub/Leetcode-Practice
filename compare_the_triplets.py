b962b89232ad148b35a6ab69b04a10a9b99d958f
ThoshHub <Shamthosh@gmail.com>
2020-10-09 23:28:42 -0400

    compare the triplets finished

--- compare_the_triplets.py
+++ compare_the_triplets.py
@@ -3,9 +3,14 @@
 def compareTriplets(a, b):
 	alice_points = 0
 	bob_points = 0
-	return [1, 1]
+	for i in range(3):
+		if a[i] > b [i]:
+			alice_points += 1
+		elif a[i] < b [i]:
+			bob_points += 1
+	return [alice_points, bob_points]
 
 a = [1, 2, 3]
-b = [3, 2, 1]
+b = [3, 4, 5]
 result = compareTriplets(a, b)
 print(result)
