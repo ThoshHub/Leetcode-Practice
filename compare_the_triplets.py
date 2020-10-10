588c3fff91566e82b654dbc9e8731ce826023e00
ThoshHub <Shamthosh@gmail.com>
2020-10-09 23:21:25 -0400

    added sudoku2

---
+++ compare_the_triplets.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/compare-the-triplets/problem?h_r=next-challenge&h_v=zen
+
+def compareTriplets(a, b):
+	alice_points = 0
+	bob_points = 0
+	return [1, 1]
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+result = compareTriplets(a, b)
+print(result)
