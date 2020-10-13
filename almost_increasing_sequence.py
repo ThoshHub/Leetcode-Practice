a04820d10772fd7669aa43bd4b2567a819b01190
ThoshHub <Shamthosh@gmail.com>
2020-10-13 19:52:57 -0400

    initial increasing sequence solution

--- almost_increasing_sequence.py
+++ almost_increasing_sequence.py
@@ -1,15 +1,11 @@
 # https://app.codesignal.com/arcade/intro/level-2/2mxbGwLzvkTCKAJMG
 
+# brute force method
 def almostIncreasingSequence(sequence):
-	count = 0
-	peak = max(sequence)
-	sequence.remove(peak)
-	print(sequence)
-	for index, object in enumerate(sequence[:len(sequence)-1]):
-		if sequence[index] >= sequence[index+1]:
-			return False
 	
-	return True
+	for i in sequence:
+		sequence.remove(sequence[0])
+		print(sequence)
 
 a = [1, 2, 3, 4, 99, 5, 6]
 print(almostIncreasingSequence(a))
