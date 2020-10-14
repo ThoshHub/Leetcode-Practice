7bb84927257879b3c2ea51f41510067652f21c19
ThoshHub <Shamthosh@gmail.com>
2020-10-13 20:08:26 -0400

    moar

--- almost_increasing_sequence.py
+++ almost_increasing_sequence.py
@@ -1,11 +1,34 @@
 # https://app.codesignal.com/arcade/intro/level-2/2mxbGwLzvkTCKAJMG
 
-# brute force method
 def almostIncreasingSequence(sequence):
-	
-	for i in sequence:
-		sequence.remove(sequence[0])
-		print(sequence)
+    #Take out the edge cases
+    if len(sequence) <= 2:
+        return True
+
+    #Set up a new function to see if it's increasing sequence
+    def IncreasingSequence(test_sequence):
+        if len(test_sequence) == 2:
+            if test_sequence[0] < test_sequence[1]:
+                return True
+        else:
+            for i in range(0, len(test_sequence)-1):
+                if test_sequence[i] >= test_sequence[i+1]:
+                    return False
+                else:
+                    pass
+            return True
+
+    for i in range (0, len(sequence) - 1):
+        if sequence[i] >= sequence [i+1]:
+            #Either remove the current one or the next one
+            test_seq1 = sequence[:i] + sequence[i+1:]
+            test_seq2 = sequence[:i+1] + sequence[i+2:]
+            if IncreasingSequence(test_seq1) == True:
+                return True
+            elif IncreasingSequence(test_seq2) == True:
+                return True
+            else:
+                return False
 
 a = [1, 2, 3, 4, 99, 5, 6]
 print(almostIncreasingSequence(a))
