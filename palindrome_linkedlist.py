bf21b45ebd7840a5cd5baa2f1bb783f0a95caf46
ThoshHub <shamthosh@gmail.com>
2021-05-03 11:52:34 -0400

    test

---
+++ palindrome_linkedlist.py
@@ -0,0 +1,7 @@
+# https://leetcode.com/explore/featured/card/april-leetcoding-challenge-2021/593/week-1-april-1st-april-7th/3693/
+head = [1, 2, 2, 1]
+head2 = [1,2]
+
+
+print("test")
+print("test")
