1c42586363a72fd128810c895e0d8586ad484609
ThoshHub <shamthosh@gmail.com>
2021-05-14 01:27:05 -0400

    gm

--- palindrome_linkedlist.py
+++ palindrome_linkedlist.py
@@ -1,9 +1,3 @@
 # https://leetcode.com/explore/featured/card/april-leetcoding-challenge-2021/593/week-1-april-1st-april-7th/3693/
 head = [1, 2, 2, 1]
 head2 = [1,2]
-
-
-print("test")
-print("test")
-print("test")
-print("test")
---
+++ quicksort_implementation.py
@@ -0,0 +1,23 @@
+import random
+
+def qsort(s):
+    # if array is 0 or 1 long, then return it
+    if(len(s) < 2): # if the 
+        return s
+    elif(len(s) == 2):
+        # check
+
+    else:
+        # otherwise will need to do recursion
+        # pick a pivot (random)
+        # take everything before the array and put it into new array "a"
+        # take everything before the array
+
+    return []
+
+a = []
+b = [1]
+c = [2, 1]
+d = [3, 5, 2, 43, 12, 4, 5, 7, 88, 432, 23]
+
+print(qsort(a))
