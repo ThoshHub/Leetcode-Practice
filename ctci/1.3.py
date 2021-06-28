fa8634e23a90ff2a51ffd12484bb8b81797505af
ThoshHub <shamthosh@gmail.com>
2021-06-28 00:09:02 -0400

    gm

---
+++ ctci/1.3.py
@@ -0,0 +1,12 @@
+# URLify: Write a method to replace all spaces in a string with '%20'. You may assume that the string
+# has sufficient space at the end to hold the additional characters, and that you are given the "true"
+# length of the string. (Note: If implementing in Java, please use a character array so that you can
+# perform this operation in place.)
+
+# Python Strings are immutable
+# This problem is not possible in python
+def v(str, l):
+	return 0
+
+str = "Mr John Smith    ", 13
+print(v(str, len(str)))
