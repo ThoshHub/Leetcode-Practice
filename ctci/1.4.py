c7f514080369602c6a415b317212f9c27b31c186
ThoshHub <shamthosh@gmail.com>
2021-06-28 00:15:41 -0400

    1.4

---
+++ ctci/1.4.py
@@ -0,0 +1,17 @@
+﻿# Given a string, write a function to check if it is a permutation of a palin
+# drome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation
+# is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.
+
+
+# strip all spaces
+# then build dictionary for each letter in input string
+# key = letter
+# value = number of times that letter shows up in the string
+# all values must be divisible by two except one
+# TODO
+def f(str):
+	return 0
+
+
+str = "abc cba"
+print(f(str))
